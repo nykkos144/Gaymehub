46e04a8685f55c80ed7b244fd2c89d0a917304de
unknown <nikolaj.kostadinov1215@gmail.com>
2022-04-01 00:19:30 +0300

    ljkhfbas commit

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname + "/public")));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 const chessIo = io.of('/CHESS');
 
