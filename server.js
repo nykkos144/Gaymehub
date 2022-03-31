734901cc0e91c4ed201cabaedd0d177d13b5c0fc
unknown <nikolaj.kostadinov1215@gmail.com>
2022-03-31 22:39:30 +0300

    zxc commit

--- server.js
+++ server.js
@@ -356,6 +356,18 @@ tttIo.on("connection", (socket) => {
 });
 
 
+app.get("*", (req, res) => {
+
+  let path = req.params['0'].substring(1)
+
+  if (protected.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/build/index.html`);
+  }
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
