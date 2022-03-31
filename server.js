24bd4465daf6c9237e57d3582bea596a62670c22
unknown <nikolaj.kostadinov1215@gmail.com>
2022-04-01 00:21:04 +0300

    ljkhffsdbas commit

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found'
+  });
+});
 
 const chessIo = io.of('/CHESS');
 
